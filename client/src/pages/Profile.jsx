21e1187b2529803e8a0bc5a83d854d7af7d5ebf9
Nitish Aggarwal <inspirednitish2018@gmail.com>
2024-04-30 18:00:58 +1000

    Created profile page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import { useSelector } from "react-redux";
 
 export default function Profile() {
+  const {currentUser} = useSelector((state) => state.user);
   return (
-    <div>Profile</div>
+    <div className="p-3 max-w-lg mx-auto gap-4">
+      <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
+      <form className="flex flex-col">
+      <img src={currentUser.profilePicture} alt="profile" className="h-24 w-24 self-center cursor-pointer rounded-full object-cover"/>
+      <input defaultValue={currentUser.username}type="text" placeholder="Username" id="username" className='bg-slate-100 p-3 rounded-lg' onChange={handleChange}/>
+      <input type="email" placeholder="Email" id="email" className='bg-slate-100 p-3 rounded-lg' onChange={handleChange}/>
+      <input type="password" placeholder="Password" id="password" className='bg-slate-100 p-3 rounded-lg' onChange={handleChange}/>
+      <button className=' bg-slate-700 uppercase text-white rounded-lg p-3 hover:opacity-95 disabled:opacity-80'>update</button>
+      </form>
+      <div className="flex justify-between mt-5">
+        <span className="text-red-700 cursor-pointer">Delete Account</span>
+        <span className="text-red-700 cursor-pointer">Sign out</span>
+      </div>
+    </div>
   )
 }
