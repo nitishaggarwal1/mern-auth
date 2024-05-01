3e63aaea2af50c3d673e430a0ea5727252c9a949
Nitish Aggarwal <inspirednitish2018@gmail.com>
2024-05-01 19:52:26 +1000

    Added pre production changes to config files

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import authRouters from './routes/auth.route.js';
 import userRouters from './routes/user.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 dotenv.config();
 
@@ -13,7 +14,15 @@ mongoose.connect(process.env.MONGO).then(() =>{
     console.log(err)
 })
 
+const __dirname = path.resolve();
+
 const app = express();
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 app.use(express.json());
 
